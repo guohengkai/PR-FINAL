0b1d3cdf9de1ca0e1e0343ca298b60a2d560d0bb
guohengkai <guohengkaighk@gmail.com>
2015-05-25 18:34:16 +0800

    Add hog classifier using opencv(fail)

---
+++ hog_sign_classifier.cpp
@@ -0,0 +1,151 @@
+/*************************************************************************
+    > File Name: hog_sign_classifier.cpp
+    > Author: Guo Hengkai
+    > Description: HOG sign classifier class implementation using SVM
+    > Created Time: Mon 25 May 2015 03:37:00 PM CST
+ ************************************************************************/
+#include "hog_sign_classifier.h"
+#include "dataset.h"
+#include "hog_extractor.h"
+#include "svm_classifier.h"
+#include "file_util.h"
+#include "test_util.h"
+
+namespace ghk
+{
+bool HogSignClassifier::Save(const string &model_name) const
+{
+    vector<float> param(1, img_size_);
+    if (!SaveMat(model_name + "_para", Mat(), param))
+    {
+        printf("Fail to save the parameter.\n");
+        return false;
+    }
+    if (!svm_classifier_.Save(model_name + "_svm"))
+    {
+        printf("Fail to save SVM.\n");
+        return false;
+    }
+    return true;
+}
+
+bool HogSignClassifier::Load(const string &model_name)
+{
+    vector<float> param;
+    Mat tmp;
+    if (!LoadMat(model_name + "_para", &tmp, &param))
+    {
+        printf("Fail to load the parameter.\n");
+        return false;
+    }
+    if (!svm_classifier_.Load(model_name + "_svm"))
+    {
+        printf("Fail to load SVM.\n");
+        return false;
+    }
+    return true;
+}
+
+bool HogSignClassifier::Train(const Dataset &dataset)
+{
+    // Get training data
+    vector<Mat> images;
+    vector<int> labels;
+    size_t n = dataset.GetClassifyNum(true);
+    Size img_size(img_size_, img_size_);
+    printf("Preparing training data...\n");
+    for (size_t i = 0; i < n; ++i)
+    {
+        Mat image;
+        dataset.GetClassifyImage(true, i, &image, img_size);
+        cv::cvtColor(image, image, CV_BGR2GRAY);
+        images.push_back(image);
+        labels.push_back(dataset.GetClassifyLabel(true, i));
+        /*
+        auto m = labels.size();
+        if (labels[m - 1] > 2)
+        {
+            images.resize(m - 1);
+            labels.resize(m - 1);
+            break;
+        }*/
+    }
+
+    // Feature extraction
+    Mat feats;
+    printf("Extracting features...\n");
+    hog_extractor_.Extract(images, &feats);
+
+    // Train the SVM classifier
+    printf("Training SVM classifier...\n");
+    svm_classifier_.Train(feats, labels);
+    printf("Training done! Now testing...\n");
+
+    // Test on training
+    vector<int> predict_labels;
+    svm_classifier_.Predict(feats, &predict_labels);
+    float rate, fp;
+    EvaluateClassify(labels, predict_labels, CLASS_NUM, false, &rate, &fp);
+    // EvaluateClassify(labels, predict_labels, 3, false, &rate, &fp);
+    printf("Test on training rate: %0.2f%%\n", rate * 100);
+
+    return true;
+}
+
+bool HogSignClassifier::Test(const Dataset &dataset)
+{
+    // Get test data
+    vector<Mat> images;
+    vector<int> labels;
+    size_t n = dataset.GetClassifyNum(false);
+    Size img_size(img_size_, img_size_);
+    printf("Preparing testing data...\n");
+    for (size_t i = 0; i < n; ++i)
+    {
+        Mat image;
+        dataset.GetClassifyImage(false, i, &image, img_size);
+        cv::cvtColor(image, image, CV_BGR2GRAY);
+        images.push_back(image);
+        labels.push_back(dataset.GetClassifyLabel(false, i));
+        /*
+        auto m = labels.size();
+        if (labels[m - 1] > 2)
+        {
+            images.resize(m - 1);
+            labels.resize(m - 1);
+            break;
+        }*/
+    }
+
+    // Test on test dataset
+    vector<int> predict_labels;
+    Predict(images, &predict_labels);
+    float rate, fp;
+    EvaluateClassify(labels, predict_labels, CLASS_NUM, false, &rate, &fp);
+    // EvaluateClassify(labels, predict_labels, 3, false, &rate, &fp);
+    printf("Test rate: %0.2f%%\n", rate * 100);
+
+    return true;
+}
+
+bool HogSignClassifier::Predict(const vector<Mat> &images,
+        vector<int> *labels)
+{
+    if (labels == nullptr)
+    {
+        return false;
+    }
+
+    // Feature extraction
+    Mat feats;
+    printf("Extracting features...\n");
+    hog_extractor_.Extract(images, &feats);
+
+    // Prediction
+    printf("Predicting with SVM...\n");
+    svm_classifier_.Predict(feats, labels);
+    printf("Prediction done!\n");
+
+    return true;
+}
+}  // namespace ghk
---
+++ hog_sign_classifier.h
@@ -0,0 +1,40 @@
+/*************************************************************************
+    > File Name: hog_sign_classifier.h
+    > Author: Guo Hengkai
+    > Description: HOG sign classifier class definition using SVM
+    > Created Time: Mon 25 May 2015 03:24:27 PM CST
+ ************************************************************************/
+#ifndef FINAL_HOG_SIGN_CLASSIFIER_H_
+#define FINAL_HOG_SIGN_CLASSIFIER_H_
+
+#include "common.h"
+#include "dataset.h"
+#include "hog_extractor.h"
+#include "svm_classifier.h"
+#include "sign_classifier.h"
+
+namespace ghk
+{
+class HogSignClassifier: public SignClassifier
+{
+public:
+    HogSignClassifier(float c = 125, int img_size = 100):
+        svm_classifier_(c), img_size_(img_size) {}
+
+    virtual bool Save(const string &model_name) const;
+    virtual bool Load(const string &model_name);
+
+    virtual bool Train(const Dataset &dataset);
+    virtual bool Test(const Dataset &dataset);
+    virtual bool Predict(const vector<Mat> &images,
+            vector<int> *labels);
+
+private:
+    HogExtractor hog_extractor_;
+    SvmClassifier svm_classifier_;
+    int img_size_;
+};
+}  // namespace ghk
+
+#endif  // FINAL_HOG_SIGN_CLASSIFIER_H_
+
